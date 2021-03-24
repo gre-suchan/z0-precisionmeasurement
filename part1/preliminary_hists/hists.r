98ddc511f2222434dd2347b5133bac1e288287e0
gre-suchan <80713427+gre-suchan@users.noreply.github.com>
2021-03-24 13:00:59 +0100

    Refine preliminary cuts
    Including calculation of acceptance loss and background of each particle

--- part1/preliminary_hists/hists.r
+++ part1/preliminary_hists/hists.r
@@ -10,6 +10,7 @@ taus      <- read.csv("../grope-tables/taus.csv")
 # Combine the data.frames
 data <- rbind(hadrons, electrons, muons, taus)
 data$particle <- rep(c("h", "e", "m", "t"), each=50)
+data$guess <- "u" # This is the guessed particle type. u="unknown"
 
 # This is the binning of the n.charged observable
 nchr.brks <- seq(0, 80, by=5)
@@ -31,7 +32,8 @@ hist(taus$n.charged,      breaks=nchr.brks, add=T, col=t.col)
 # }}}
 
 # Cut: hadrons have n.charged >= 15
-sum(hadrons$n.charged >= 15) / length(hadrons$n.charged)
+sum(data$particle == "h" & data$n.charged >= 15) / sum(data$particle == "h")
+data[data$n.charged >= 15, ]$guess <- "h"
 
 # New Criterion: sump/e.ecal
 hadrons$tec.ratio   <- hadrons$p.charged   / hadrons$e.ecal
@@ -41,6 +43,8 @@ taus$tec.ratio      <- taus$p.charged      / taus$e.ecal
 data$tec.ratio      <- data$p.charged      / data$e.ecal
 
 # "Only" muons have a high tex.ratio, i.e. sump/e.ecal
+sum(data$particle == "m" & data$tec.ratio >= 8) / sum(data$particle == "m")
+data[data$guess == "u" & data$tec.ratio >= 8,]$guess <- "m"
 
 tex.brks <- c(seq(0, 7.5, by=.5), seq(8, 75, by=5))
 ggplot(data=data[data$particle != "h", ],
@@ -52,12 +56,24 @@ hist(electrons$tec.ratio, breaks=tex.brks,freq=T, ylim=c(0, 60), xlim=c(0, 8.5),
 hist(muons$tec.ratio,     breaks=tex.brks,freq=T, ylim=c(0, 60), xlim=c(0, 8.5), add=T, col=m.col)
 hist(taus$tec.ratio,      breaks=tex.brks,freq=T, ylim=c(0, 60), xlim=c(0, 8.5), add=T, col=t.col)
 
-# Test:
-sum(muons$tec.ratio >= 8) / length(muons$tec.ratio)
 # Maybe attempt to cut e.ecal to separate electrons and taus?
 ggplot(data=data[data$particle %in% c("e", "t"), ],
-       aes(x=e.ecal, group=particle, fill=particle)) +
-    geom_histogram(alpha=.5, breaks=seq(0, 125, by=5), position='identity', color='black') +
-    theme_minimal()
+       aes(x=e.hcal, fill=particle)) +
+    geom_histogram(breaks=seq(0, 125, by=5), position='identity', color='grey50', alpha=.5) +
+    theme_classic()
+
+data[data$guess == "u" & data$e.ecal >= 70 & data$e.hcal <= 20,]$guess <- "e"
+data[data$guess == "u" & data$e.ecal <= 60,]$guess <- "t"
+
+
+# Now we calculate the background and acceptance of each particle
+for (particle in c("h", "m", "e", "t")) {
+    print(paste("Acceptance loss for", particle))
+    print(1 - sum(data$particle == particle & data$guess == particle) /
+          sum(data$particle == particle))
+    print(paste("Background for", particle))
+    print(sum(data$particle != particle & data$guess == particle) /
+          sum(data$guess == particle))
+}
 
 # vim:foldmethod=marker
