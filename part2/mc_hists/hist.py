68542b02b77f3cd3dbd573073070eecea75db76f
MaltePhysik <64465218+MaltePhysik@users.noreply.github.com>
2021-03-24 12:10:41 +0100

    Hists for the mc data.

---
+++ part2/mc_hists/hist.py
@@ -0,0 +1,41 @@
+import uproot
+import awkward as ak
+import numpy as np
+import matplotlib.pyplot as plt
+
+path_data = '../../z0decayMC/'
+
+file = uproot.open(path_data + 'ee.root')
+ttree_name = 'myTTree'
+
+file[ttree_name].keys()
+
+branches = file[ttree_name].arrays()
+
+var = 'E_ecal'
+e_ecal = ak.to_numpy(branches[var])
+var4 = 'E_hcal'
+e_hcal = ak.to_numpy(branches[var4])
+
+file2 = uproot.open(path_data + 'tt.root')
+
+ttree_name2 = 'myTTree'
+
+file2[ttree_name2].keys()
+
+branches = file2[ttree_name2].arrays()
+
+var2 = 'E_ecal'
+e_ecal2 = ak.to_numpy(branches[var2])
+var3 = 'E_hcal'
+e_hcal2 = ak.to_numpy(branches[var3])
+
+
+#plt.hist(e_hcal, bins = [0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50])
+#plt.hist(e_hcal2, bins = [0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50],alpha=0.7)
+#plt.show()
+
+plt.hist(e_ecal, bins = [0,10,20,30,40,50,60,70,80,90,100,110,120,130])
+plt.hist(e_ecal2, bins = [0,10,20,30,40,50,60,70,80,90,100,110,120,130], alpha=0.7)
+plt.show()
+
