d3ce95f4ab92dc7f09bad19c22bd9bb42618654a
gre-suchan <80713427+gre-suchan@users.noreply.github.com>
2021-03-27 12:06:36 +0100

    Cleanup s_t_channel.py

--- part2/hists/s_t_channel.py
+++ part2/hists/s_t_channel.py
@@ -1,19 +1,21 @@
 import pandas as pd
 from cuts import mc_df as df
-from cosfit import upper_cut as upper_cut
-from cosfit import lower_cut as lower_cut 
+from cosfit import upper_cut
+from cosfit import lower_cut
+
 
 def apply_stcut(data: pd.DataFrame):
     """Apply the cuts for the s channel to the otherwise already cuted data."""
-    
+
     # Adding a new column defining if it is a s channel electron or not.
     data['s_channel'] = 'u'
-    
+
     # Doing the actual cut, by now cutting out the part in between -0.9 and 0,
     # which hopefully is a convenient tradeoff between efficiency and data
     # loss.
-    data.loc[(data['s_channel'] == 'u') & (data['guess'] == 'e') &
-            (data['cos_thet'] >= lower_cut) & (data['cos_thet'] <= upper_cut), 's_channel'] = 's'
+    data.loc[(data['guess'] == 'e') & (data['cos_thet'] >= lower_cut) &
+             (data['cos_thet'] <= upper_cut), 's_channel'] = 's'
+
 
 final_df = df
 apply_stcut(final_df)
