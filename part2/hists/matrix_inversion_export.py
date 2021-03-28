6483e063e516abc6cb1455ff25704117c1277026
gre-suchan <80713427+gre-suchan@users.noreply.github.com>
2021-03-28 14:33:14 +0200

    Add a way to export the efficiency matrix as plots
    Note that the .txt files in this commit will probably change when
    re-running matrix_inversion_export.py as this is essentially randomly
    generated data.
    
    So please don't do that.

---
+++ part2/hists/matrix_inversion_export.py
@@ -0,0 +1,37 @@
+"""
+Exports the efficiency matrix and their errors to a .txt file.
+Note that running this file probably changes the .txt files indexed in the repo
+so make sure not to commit changes to the .txt files
+"""
+import numpy as np
+from os import path
+from matrix_inversion import hists, inverse_means, inverse_errors
+
+PATH = '../../plot_data/part2/efficiencies/'
+assert path.exists(PATH)
+
+
+def export_hist(h: np.array, file, header=''):
+    """Saves a matplotlib hist object as tabular txt
+
+    :h: The matplotlib histogram
+    :file: The name of the output file
+    :header: The header of the txt
+    :returns: None
+
+    """
+    np.savetxt(file,
+               np.array(list(zip((h[1][:-1] + h[1][1:]) / 2, h[0]))),
+               header=header,
+               comments='')
+
+
+for j in range(4):
+    for k in range(4):
+        export_hist(hists[j][k], PATH + f'{j}{k}.txt')
+
+with open(PATH + 'fit_means.txt', 'w') as f:
+    f.write("{{\"" + "\",\"".join(map(str, inverse_means.flatten())) + "\"}}")
+
+with open(PATH + 'fit_sds.txt', 'w') as f:
+    f.write("{{\"" + "\",\"".join(map(str, inverse_errors.flatten())) + "\"}}")
