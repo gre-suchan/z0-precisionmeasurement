Correct calculation of efficiencies

part2/hists/efficiencies.py
@@ -11,7 +11,7 @@ err_mat = np.zeros((4, 4))
 for i, p1 in enumerate(ptypes):
     for j, p2 in enumerate(ptypes):
         num = sum((df['guess'] == p2) & (df['ptype'] == p1))
-        denom = sum(df['ptype'] == p2)
+        denom = 100000  # Weird detector shit
         mat[i, j] = num / denom
         err_mat[i, j] = np.sqrt(num / denom**2 + num**2 / denom**3)
 

part2/hists/matrix_inversion.py
@@ -91,8 +91,10 @@ for alpha in range(4):
                 for i in range(4) for j in range(4)))
 
 if __name__ == "__main__":
-    print(f"Erros for the inverse matrix:\n{inverse_errors}")
     print(f"Means for the inverse matrix:\n{inverse_means}")
+    print(f"Erros for the inverse matrix:\n{inverse_errors}")
     plt.show()
+    print("Inverse")
+    print(np.linalg.inv(mat))
     print("Exact solution")
     print(exact_inverse_errors)