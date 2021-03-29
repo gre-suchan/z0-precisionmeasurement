17df73e1e22e94338b167774535dd88b5da275bb
gre-suchan <80713427+gre-suchan@users.noreply.github.com>
2021-03-29 16:10:23 +0200

    Cleanup forward_backward.py

--- part2/hists/forward_backward.py
+++ part2/hists/forward_backward.py
@@ -5,13 +5,16 @@ from cuts import opal_df
 
 # Read in the luminosity data
 lum = pd.read_csv("../../z0DecayData/lumi_files/daten_3.csv")
-#print(lum)
+# print(lum)
 
 # Function to calculate the forward-backward asymmetry for given forward and
 # backward cross sections. sig_b refers to the backward and sig_f to the
 # forward hemisperes.
+
+
 def A_FB(sig_b, sig_f):
     return (sig_f - sig_b) / (sig_f + sig_b)
+
 
 #### MC Data ####
 
@@ -19,18 +22,17 @@ def A_FB(sig_b, sig_f):
 mc_df = get_mc_dataframe()
 
 # Cutting out the events of the forward and backward hemisphere respectly.
-mc_muon_b = mc_df.loc[(mc_df['ptype'] == 'm') & (mc_df['cos_thet'] < 0) & (mc_df['cos_thet']
-    > -1)]
+mc_muon_b = mc_df.loc[(mc_df['ptype'] == 'm') & (mc_df['cos_thet'] < 0) &
+                      (mc_df['cos_thet'] > -1)]
 mc_muon_f = mc_df.loc[(mc_df['ptype'] == 'm') & (mc_df['cos_thet'] < 1) &
-    (mc_df['cos_thet'] > 0)]
+                      (mc_df['cos_thet'] > 0)]
 
-#print(mc_muon_b)
+# print(mc_muon_b)
 
 # Calculating the forward and backward cross section using the luminosity for
 # the nearest beam energy to the one used in the mc simulations.
 mc_sig_b = (1 / lum['lumi'][3]) * len(mc_muon_b)
 mc_sig_f = (1 / lum['lumi'][3]) * len(mc_muon_f)
-#print(mc_sig_b, mc_sig_f)
 
 # Calculating the forward-backward asymmetry for the mc data.
 mc_afb = A_FB(mc_sig_b, mc_sig_f)
@@ -40,44 +42,58 @@ print(mc_afb)
 
 # Cutting out the events of the forward and backward hemisphere respectly for
 # each beam energy.
-op_muon_b0 = opal_df.loc[(opal_df['guess'] == 'm') & (opal_df['cos_thet'] < 0) &
-    (opal_df['cos_thet'] > -1) & (opal_df['E_lep']*2 < (89.46658+88.47630)/2)]
-op_muon_f0 = opal_df.loc[(opal_df['guess'] == 'm') & (opal_df['cos_thet'] < 1) &
-    (opal_df['cos_thet'] > 0) & (opal_df['E_lep']*2 < (89.46658+88.47630)/2)]
-op_muon_b1 = opal_df.loc[(opal_df['guess'] == 'm') & (opal_df['cos_thet'] < 0) &
-    (opal_df['cos_thet'] > -1) & (opal_df['E_lep']*2 > (89.46658+88.47630)/2) &
-    (opal_df['E_lep']*2 < (89.46658+90.21986)/2)]
-op_muon_f1 = opal_df.loc[(opal_df['guess'] == 'm') & (opal_df['cos_thet'] < 1) &
-    (opal_df['cos_thet'] > 0) & (opal_df['E_lep']*2 > (89.46658+88.47630)/2) &
-    (opal_df['E_lep']*2 < (89.46658+90.21986)/2)]
-op_muon_b2 = opal_df.loc[(opal_df['guess'] == 'm') & (opal_df['cos_thet'] < 0) &
-    (opal_df['cos_thet'] > -1) & (opal_df['E_lep']*2 > (89.46658+90.21986)/2) &
-    (opal_df['E_lep']*2 < (91.22910+90.21986)/2)]
-op_muon_f2 = opal_df.loc[(opal_df['guess'] == 'm') & (opal_df['cos_thet'] < 1) &
-    (opal_df['cos_thet'] > 0) & (opal_df['E_lep']*2 > (89.46658+90.21986)/2) &
-    (opal_df['E_lep']*2 < (91.22910+90.21986)/2)]
-op_muon_b3 = opal_df.loc[(opal_df['guess'] == 'm') & (opal_df['cos_thet'] < 0) &
-    (opal_df['cos_thet'] > -1) & (opal_df['E_lep']*2 > (91.22910+90.21986)/2) &
-    (opal_df['E_lep']*2 < (91.22910+91.96428)/2)]
-op_muon_f3 = opal_df.loc[(opal_df['guess'] == 'm') & (opal_df['cos_thet'] < 1) &
-    (opal_df['cos_thet'] > 0) & (opal_df['E_lep']*2 > (91.22910+90.21986)/2) &
-    (opal_df['E_lep']*2 < (91.22910+91.96428)/2)]
-op_muon_b4 = opal_df.loc[(opal_df['guess'] == 'm') & (opal_df['cos_thet'] < 0) &
-    (opal_df['cos_thet'] > -1) & (opal_df['E_lep']*2 > (91.22910+91.96428)/2) &
-    (opal_df['E_lep']*2 < (92.96229+91.96428)/2)]
-op_muon_f4 = opal_df.loc[(opal_df['guess'] == 'm') & (opal_df['cos_thet'] < 1) &
-    (opal_df['cos_thet'] > 0) & (opal_df['E_lep']*2 > (91.22910+91.96428)/2) &
-    (opal_df['E_lep']*2 < (92.96229+91.96428)/2)]
-op_muon_b5 = opal_df.loc[(opal_df['guess'] == 'm') & (opal_df['cos_thet'] < 0) &
-    (opal_df['cos_thet'] > -1) & (opal_df['E_lep']*2 > (92.96229+91.96428)/2) &
-    (opal_df['E_lep']*2 < (92.96229+93.71362)/2)]
-op_muon_f5 = opal_df.loc[(opal_df['guess'] == 'm') & (opal_df['cos_thet'] < 1) &
-    (opal_df['cos_thet'] > 0) & (opal_df['E_lep']*2 > (92.96229+91.96428)/2) &
-    (opal_df['E_lep']*2 < (92.96229+93.71362)/2)]
-op_muon_b6 = opal_df.loc[(opal_df['guess'] == 'm') & (opal_df['cos_thet'] < 0) &
-    (opal_df['cos_thet'] > -1) & (opal_df['E_lep']*2 > (92.96229+93.71362)/2)]
-op_muon_f6 = opal_df.loc[(opal_df['guess'] == 'm') & (opal_df['cos_thet'] < 1) &
-    (opal_df['cos_thet'] > 0) & (opal_df['E_lep']*2 > (92.96229+93.71362)/2)]
+op_muon_b0 = opal_df.loc[(opal_df['guess'] == 'm') & (opal_df['cos_thet'] < 0)
+                         & (opal_df['cos_thet'] > -1) &
+                         (opal_df['E_lep'] * 2 < (89.46658 + 88.47630) / 2)]
+op_muon_f0 = opal_df.loc[(opal_df['guess'] == 'm') & (opal_df['cos_thet'] < 1)
+                         & (opal_df['cos_thet'] > 0) &
+                         (opal_df['E_lep'] * 2 < (89.46658 + 88.47630) / 2)]
+op_muon_b1 = opal_df.loc[(opal_df['guess'] == 'm') & (opal_df['cos_thet'] < 0)
+                         & (opal_df['cos_thet'] > -1) &
+                         (opal_df['E_lep'] * 2 > (89.46658 + 88.47630) / 2) &
+                         (opal_df['E_lep'] * 2 < (89.46658 + 90.21986) / 2)]
+op_muon_f1 = opal_df.loc[(opal_df['guess'] == 'm') & (opal_df['cos_thet'] < 1)
+                         & (opal_df['cos_thet'] > 0) &
+                         (opal_df['E_lep'] * 2 > (89.46658 + 88.47630) / 2) &
+                         (opal_df['E_lep'] * 2 < (89.46658 + 90.21986) / 2)]
+op_muon_b2 = opal_df.loc[(opal_df['guess'] == 'm') & (opal_df['cos_thet'] < 0)
+                         & (opal_df['cos_thet'] > -1) &
+                         (opal_df['E_lep'] * 2 > (89.46658 + 90.21986) / 2) &
+                         (opal_df['E_lep'] * 2 < (91.22910 + 90.21986) / 2)]
+op_muon_f2 = opal_df.loc[(opal_df['guess'] == 'm') & (opal_df['cos_thet'] < 1)
+                         & (opal_df['cos_thet'] > 0) &
+                         (opal_df['E_lep'] * 2 > (89.46658 + 90.21986) / 2) &
+                         (opal_df['E_lep'] * 2 < (91.22910 + 90.21986) / 2)]
+op_muon_b3 = opal_df.loc[(opal_df['guess'] == 'm') & (opal_df['cos_thet'] < 0)
+                         & (opal_df['cos_thet'] > -1) &
+                         (opal_df['E_lep'] * 2 > (91.22910 + 90.21986) / 2) &
+                         (opal_df['E_lep'] * 2 < (91.22910 + 91.96428) / 2)]
+op_muon_f3 = opal_df.loc[(opal_df['guess'] == 'm') & (opal_df['cos_thet'] < 1)
+                         & (opal_df['cos_thet'] > 0) &
+                         (opal_df['E_lep'] * 2 > (91.22910 + 90.21986) / 2) &
+                         (opal_df['E_lep'] * 2 < (91.22910 + 91.96428) / 2)]
+op_muon_b4 = opal_df.loc[(opal_df['guess'] == 'm') & (opal_df['cos_thet'] < 0)
+                         & (opal_df['cos_thet'] > -1) &
+                         (opal_df['E_lep'] * 2 > (91.22910 + 91.96428) / 2) &
+                         (opal_df['E_lep'] * 2 < (92.96229 + 91.96428) / 2)]
+op_muon_f4 = opal_df.loc[(opal_df['guess'] == 'm') & (opal_df['cos_thet'] < 1)
+                         & (opal_df['cos_thet'] > 0) &
+                         (opal_df['E_lep'] * 2 > (91.22910 + 91.96428) / 2) &
+                         (opal_df['E_lep'] * 2 < (92.96229 + 91.96428) / 2)]
+op_muon_b5 = opal_df.loc[(opal_df['guess'] == 'm') & (opal_df['cos_thet'] < 0)
+                         & (opal_df['cos_thet'] > -1) &
+                         (opal_df['E_lep'] * 2 > (92.96229 + 91.96428) / 2) &
+                         (opal_df['E_lep'] * 2 < (92.96229 + 93.71362) / 2)]
+op_muon_f5 = opal_df.loc[(opal_df['guess'] == 'm') & (opal_df['cos_thet'] < 1)
+                         & (opal_df['cos_thet'] > 0) &
+                         (opal_df['E_lep'] * 2 > (92.96229 + 91.96428) / 2) &
+                         (opal_df['E_lep'] * 2 < (92.96229 + 93.71362) / 2)]
+op_muon_b6 = opal_df.loc[(opal_df['guess'] == 'm') & (opal_df['cos_thet'] < 0)
+                         & (opal_df['cos_thet'] > -1) &
+                         (opal_df['E_lep'] * 2 > (92.96229 + 93.71362) / 2)]
+op_muon_f6 = opal_df.loc[(opal_df['guess'] == 'm') & (opal_df['cos_thet'] < 1)
+                         & (opal_df['cos_thet'] > 0) &
+                         (opal_df['E_lep'] * 2 > (92.96229 + 93.71362) / 2)]
 
 # Calculating the forward and backward cross section.
 op_sig_b = []
@@ -97,13 +113,11 @@ op_sig_f.append((1 / lum['lumi'][5]) * len(op_muon_f5))
 op_sig_b.append((1 / lum['lumi'][6]) * len(op_muon_b6))
 op_sig_f.append((1 / lum['lumi'][6]) * len(op_muon_f6))
 
-
 # Calculating the forward-backward asymmetry for the mc data.
 op_afb = []
-for i in range(0,7):
+for i in range(0, 7):
     op_afb.append(A_FB(op_sig_b[i], op_sig_f[i]))
 print(op_afb)
 
 plt.plot(lum['meanenergy'], op_afb)
 plt.show()
-
